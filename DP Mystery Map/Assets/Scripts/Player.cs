8046b2802b0000bebf2940f84692aed88ec59fa3
kenneth-w-chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2023-09-26 21:26:59 -0500

    Combined grid and free movement scripts

---
+++ DP Mystery Map/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace PlayerInfo
+{
+    /// <summary>
+    /// Enum that defines player's facing direction
+    /// </summary>
+    public enum Direction
+    {
+        None = 0,
+        Up = 1,
+        Down = 2,
+        Left = 3,
+        Right = 4
+    }
+
+    /// <summary>
+    /// Event handler delegate that is fired when the player's direction changes.
+    /// </summary>
+    /// <param name="oldValue">The <see cref="Direction"/> before the change</param>
+    /// <param name="newValue">The <see cref="Direction"/> after the change</param>
+    /// <seealso cref="DirectionChangeEventHandler"/>
+    public delegate void DirectionChangeEventHandler(Direction oldValue, Direction newValue);
+    
+    /// <summary>
+    /// This class holds settings and statuses for the player. Does not inherit MonoBehavior and should not use any Unity objects.
+    /// </summary>
+    public static class Player
+    {
+        
+        public static KeyCode MoveUpKey = KeyCode.W;
+        public static KeyCode MoveDownKey = KeyCode.S;
+        public static KeyCode MoveLeftKey = KeyCode.A;
+        public static KeyCode MoveRightKey = KeyCode.D;
+        
+        /// <summary>
+        /// The direction the player is facing
+        /// </summary>
+        private static Direction _facingDirection = Direction.Up;
+        
+        /// <summary>
+        /// Subscribe to this event to execute a command when the direction the player faces changes.
+        /// This only fires when the direction changes. It does not fire whenever the FacingDirection variable is set. 
+        /// </summary>
+        public static event DirectionChangeEventHandler DirectionChangeEvent;
+        /// <summary>
+        /// Gets or sets the player's facing direction information. Fires a <see cref="DirectionChangeEvent"/> if the value is set to a different value.
+        /// </summary>
+        public static Direction FacingDirection
+        {
+            get => _facingDirection;
+            set
+            {
+                if (_facingDirection == value)
+                    return;
+                Direction oldValue = _facingDirection;
+                _facingDirection = value;
+                DirectionChangeEvent?.Invoke(oldValue, _facingDirection);
+            }
+        }
+    }
+}
--- DP Mystery Map/Assets/Scripts/PlayerController.cs
+++ DP Mystery Map/Assets/Scripts/PlayerController.cs
@@ -1,43 +1,239 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using PlayerInfo;
 using UnityEngine;
 
+/// <summary>
+/// This script includes movement and interact colliders.
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D playerRigidbody;
     public Transform interactColliderTransform;
     public float speed = 5f;
+    
+    private MovePlayerUpdate _movePlayerUpdate;
+    private MovePlayerFixedUpdate _movePlayerFixedUpdate;
+
+    private delegate void MovePlayerUpdate();
+
+    private delegate void MovePlayerFixedUpdate();
+
+    private bool _isGridMovement;
+    private float _xMovement;
+    private float _yMovement;
+    private float _tanSpeed;
+
+    public bool IsGridMovement
+    {
+        get => _isGridMovement;
+        set
+        {
+            if (value == _isGridMovement) return;
+            if (value)
+            {
+                _movePlayerUpdate = GridMoveUpdate;
+                _movePlayerFixedUpdate = GridMoveFixedUpdate;
+                InitializeGridPosition();
+                _isGridMovement = true;
+            }
+            else
+            {
+                _movePlayerUpdate = FreeMoveUpdate;
+                _movePlayerFixedUpdate = FreeMoveFixedUpdate;
+                _isGridMovement = false;
+            }
+        }
+    }
+    
+    //Todo sort these
+    private const float WalkDuration = .25f;
+    private const float StepSize = 1f;
+    private const float KeyPushedDuration = .25f;
+
+    //_walkOn determines if the rigidbody should keep moving once it reaches its position
+    private bool _walkOn = false;
+    private bool _walkOnce = false;
+    private float _walkStartTime;
+
+    // The direction the player will start walking in. Not necessarily the direction they're facing, until they start walking
+    private Direction _walkDirection;
+    private Vector2 _startPos;
+    private Vector2 _endPos;
+    private bool _keyHeld = false;
+    private float _upKeyHeldStartTime;
+    private float _downKeyHeldStartTime;
+    private float _leftKeyHeldStartTime;
+    private float _rightKeyHeldStartTime;
+    private Dictionary<Direction, KeyCode> _directionToKeyCode;
+    
     public KeyCode moveUpKey = KeyCode.W;
     public KeyCode moveDownKey = KeyCode.S;
     public KeyCode moveLeftKey = KeyCode.A;
     public KeyCode moveRightKey = KeyCode.D;
 
-    private float _tanSpeed;
+    [NonSerialized] public bool Walking = false;
 
-    private float xMovement;
-    private float yMovement;
+    [NonSerialized] public ulong StepsTaken = 0;
+//End todo
     
-    public enum Direction
-    {
-        None = 0,
-        Up = 1,
-        Down = 2,
-        Left = 3,
-        Right = 4
-    }
-
-    [NonSerialized] public Direction FacingDirection;
     // Start is called before the first frame update
     void Start()
     {
+        _movePlayerUpdate = GridMoveUpdate;
+        _movePlayerFixedUpdate = GridMoveFixedUpdate;
+        _isGridMovement = true;
+        
+        // Initialization of free movement values
         _tanSpeed = speed * .707f;
+        
+        //initialization of
+        InitializeGridPosition();
+        _directionToKeyCode = new Dictionary<Direction, KeyCode>()
+        {
+            { Direction.Up, Player.MoveUpKey},
+            { Direction.Down, Player.MoveDownKey},
+            { Direction.Left, Player.MoveLeftKey},
+            { Direction.Right, Player.MoveRightKey }
+        };
     }
 
+    // Update is called once per frame
     void Update()
     {
+        _movePlayerUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        _movePlayerFixedUpdate();
+    }
+    
+    private void FreeMoveUpdate()
+    {
+        // Intentionally empty
+    }
+    
+    private void GridMoveUpdate()
+    {
+        Debug.Log("called");
+        //check if key was pressed down
+        if (_keyHeld)
+        {
+            //calculate time since the key was pressed
+            var timeSincePress = Time.time - _walkDirection switch
+            {
+                Direction.Up => _upKeyHeldStartTime,
+                Direction.Down => _downKeyHeldStartTime,
+                Direction.Left => _leftKeyHeldStartTime,
+                Direction.Right => _rightKeyHeldStartTime,
+                _ => 0
+            };
+            
+            //check if the key has been released
+            if (Input.GetKeyUp(_directionToKeyCode[_walkDirection]))
+            {
+                _keyHeld = false;
+                _walkOn = false;
+                if (timeSincePress <= KeyPushedDuration)
+                {
+                    if (Player.FacingDirection == _walkDirection)
+                    {
+                        UpdateMovementVals();
+                        _walkOnce = true;
+                    }
+                }
+                else
+                {
+                }
+
+                /*else FacingDirection = _walkDirection;*/
+            }
+            //else we check to see if enough time has passed to consider the button held down
+            else if (timeSincePress > KeyPushedDuration)
+            {
+                /*Debug.Log("Turning on walk");*/
+                _walkOn = true;
+                Player.FacingDirection = _walkDirection;
+            }
+
+            CheckKeys();
+        }
+        //else check for key being pressed down
+        else
+        {
+            CheckKeys();
+        }
+    }
+
+    private void FreeMoveFixedUpdate()
+    {
+        _xMovement = 0;
+        _yMovement = 0;
+        if (Input.GetKey(Player.MoveUpKey)) _yMovement += 1f;
+        if (Input.GetKey(Player.MoveDownKey)) _yMovement -= 1f;
+        if (Input.GetKey(Player.MoveRightKey)) _xMovement += 1f;
+        if (Input.GetKey(Player.MoveLeftKey)) _xMovement -= 1f;
+        
+        // Check if the player is moving diagonally
+        var absX = Mathf.Abs(_xMovement);
+        var absY = Mathf.Abs(_yMovement);
+        if (absY > 0.5f)
+        {
+            Player.FacingDirection = _yMovement > 0f ? Direction.Up : Direction.Down;
+            if (absX > 0.5f)
+            {
+                _xMovement *= _tanSpeed;
+                _yMovement *= _tanSpeed;
+            }
+            else
+            {
+                _yMovement *= speed;
+            }
+        }
+        else if (absX > 0.5f)
+        {
+            Player.FacingDirection = _xMovement > 0f ? Direction.Right : Direction.Left;
+            _xMovement *= speed;
+        }
+
+        playerRigidbody.velocity = new Vector2(_xMovement, _yMovement);
+    }
+    
+    private void GridMoveFixedUpdate()
+    {
+        if (Walking)
+        {
+            playerRigidbody.MovePosition(Vector2.Lerp(_startPos, _endPos, (Time.time - _walkStartTime) / WalkDuration));
+            if (playerRigidbody.position == _endPos)
+            {
+                Walking = false;
+                StepsTaken++;
+            }
+
+            _walkOnce = false;
+        }
+        else if (_walkOn || _walkOnce)
+        {
+            Walking = true;
+            UpdateMovementVals();
+        }
+        /*if(_walkOn)
+        // sets the player's position and then checks if they're at the end position
+        {
+            rigidBody.MovePosition(Vector2.Lerp(_startPos, _endPos, (Time.time - _startTime) / WalkDuration));
+            if (rigidBody.position == _endPos)
+            {
+                _walkOn = false;
+            }
+        }*/
+    }
+    
+    void UpdateColliders()
+    {
         // For rotating the interact collider to the side the player is facing
-        interactColliderTransform.transform.localRotation = Quaternion.Euler(0, 0, FacingDirection switch
+        interactColliderTransform.transform.localRotation = Quaternion.Euler(0, 0, Player.FacingDirection switch
         {
             Direction.Up => 0,
             Direction.Down => 180,
@@ -47,37 +243,85 @@ public class PlayerController : MonoBehaviour
         });
     }
 
-    void FixedUpdate()
+    private void InitializeGridPosition()
     {
-        xMovement = 0;
-        yMovement = 0;
-        if (Input.GetKey(moveUpKey)) yMovement += 1f;
-        if (Input.GetKey(moveDownKey)) yMovement -= 1f;
-        if (Input.GetKey(moveRightKey)) xMovement += 1f;
-        if (Input.GetKey(moveLeftKey)) xMovement -= 1f;
-        
-        // Check if the player is moving diagonally
-        var absX = Mathf.Abs(xMovement);
-        var absY = Mathf.Abs(yMovement);
-        if (absY > 0.5f)
+        var position = transform.position;
+        position = new Vector3(Mathf.Floor(position.x) + 0.5f, Mathf.Floor(position.y) + 0.5f,
+            position.z);
+        transform.position = position;
+        _startPos = _endPos = position;
+    }
+    
+    //checks if movement keys are pressed down
+    private void CheckKeys()
+    {
+        if (Input.GetKeyDown(moveRightKey))
         {
-            FacingDirection = yMovement > 0f ? Direction.Up : Direction.Down;
-            if (absX > 0.5f)
-            {
-                xMovement *= _tanSpeed;
-                yMovement *= _tanSpeed;
-            }
-            else
-            {
-                yMovement *= speed;
-            }
-        }
-        else if (absX > 0.5f)
-        {
-            FacingDirection = xMovement > 0f ? Direction.Right : Direction.Left;
-            xMovement *= speed;
+            _walkDirection = Direction.Right;
+            _keyHeld = true;
+            _rightKeyHeldStartTime = Time.time;
         }
 
-        playerRigidbody.velocity = new Vector2(xMovement, yMovement);
+        if (Input.GetKeyDown(moveLeftKey))
+        {
+            _walkDirection = Direction.Left;
+            _keyHeld = true;
+            _leftKeyHeldStartTime = Time.time;
+        }
+
+        if (Input.GetKeyDown(moveDownKey))
+        {
+            _walkDirection = Direction.Down;
+            _keyHeld = true;
+            _downKeyHeldStartTime = Time.time;
+        }
+
+        if (Input.GetKeyDown(moveUpKey))
+        {
+            _walkDirection = Direction.Up;
+            _keyHeld = true;
+            _upKeyHeldStartTime = Time.time;
+        }
     }
-}
+
+    /*private Direction CheckKeys()
+    {
+        if (Input.GetKey(moveUpKey) || Input.GetKeyDown(moveUpKey))
+        {
+            //move up
+            return Direction.Up;
+        }
+        if (Input.GetKey(moveDownKey) || Input.GetKeyDown(moveDownKey))
+        {
+            return Direction.Down;
+        }
+        if (Input.GetKey(moveLeftKey) || Input.GetKeyDown(moveLeftKey))
+        {
+         return Direction.Left;
+        }
+        if (Input.GetKey(moveRightKey) || Input.GetKeyDown(moveRightKey))
+        {
+            
+            return Direction.Right;
+        }
+        else
+        {
+            return Direction.None;
+        }
+    }
+
+    */
+    private void UpdateMovementVals()
+    {
+        _walkStartTime = Time.time;
+        _startPos = transform.position;
+        _endPos = _walkDirection switch
+        {
+            Direction.Up => _startPos + StepSize * Vector2.up,
+            Direction.Down => _startPos + StepSize * Vector2.down,
+            Direction.Left => _startPos + StepSize * Vector2.left,
+            Direction.Right => _startPos + StepSize * Vector2.right,
+            _ => _endPos
+        };
+    }
+} 
