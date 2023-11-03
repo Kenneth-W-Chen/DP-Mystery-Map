5c322d604901ce9c6e6b16bdd0ce64d8b3211029
DavidOk14 <114937803+DavidOk14@users.noreply.github.com>
2023-11-03 07:00:13 -0500

    Interactivity completed for NPC/Items, will need updates and tweaks for further interactions per certain item and NPC i.e. carrying out certain logic per item/npc, storing in 'pocket/backpack', if upon interaction prompt appears on screen, etc. TL;DR currently differentiates between if facing an NPC, item, or nothing, needs more logic for future

--- DP Mystery Map/Assets/Scripts/InteractivitySystem.cs
+++ DP Mystery Map/Assets/Scripts/InteractivitySystem.cs
@@ -4,38 +4,83 @@ using UnityEngine;
 
 public class InteractivitySystem : MonoBehaviour
 {
-    public Transform objPoint;
-    private const float objSize = 0.2f;
-    public LayerMask objLayer;
+    /// <summary>
+    /// Player Based Interaction Components
+    /// </summary>
+    public KeyCode interactKey = KeyCode.P;
+    public Collider2D interactCollider;
 
+    /// <summary>
+    /// Detection/Flags for Interaction
+    /// </summary>
+    private bool npcCollision = false;
+    private bool itemCollision = false;
+    private GameObject itemObject = null;
+    private GameObject npcObject = null;
+
+    //Set update to constantly check for player input
     void Update()
     {
-        if(returnInteraction())
+        if (Input.GetKeyUp(interactKey))
         {
-            Debug.Log("Interact");
+            interactionType();
         }
     }
 
-    bool returnInteraction()
-    {
-        return Input.GetKeyUp(KeyCode.P);
-    }
-
-    //Based on situation will handle what type of interaction to carry-out
-    void interactionType()
+    // Based on the situation, the function will handle what type of interaction to carry out
+    public void interactionType()
     {
 
+        if (itemCollision)
+            itemInteraction();
+        else if (npcCollision)
+            npcInteraction();
+        else
+            Debug.Log("There is nothing here!");
     }
 
     //Logic for NPC interaction
     void npcInteraction()
     {
-
+        Debug.Log("Interacting with an NPC.");
+        npcObject = null;
     }
 
     //Logic for item interaction
-    void itemInteraction()
+    private void itemInteraction()
     {
+        Debug.Log("Interacting with item.");
+        Destroy(itemObject);
+        itemObject = null;
+    }
 
+    /// <summary>
+    /// Updates collision flags per walking towards and walking away
+    /// </summary>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            itemCollision = true;
+            itemObject = collision.gameObject;
+        }
+        else if (collision.gameObject.CompareTag("NPC"))
+        {
+            npcCollision = true;
+            npcObject = collision.gameObject;
+        }
+        else
+        {
+            itemCollision = false;
+            npcCollision = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (itemCollision && collision.gameObject.CompareTag("Item"))
+            itemCollision = false;
+        else if (npcCollision && collision.gameObject.CompareTag("NPC"))
+            npcCollision = false;
     }
 }
