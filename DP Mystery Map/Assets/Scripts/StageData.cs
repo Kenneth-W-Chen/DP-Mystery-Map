8bbc2dc9a571d533a2ac761fb23ddb98b791cb34
kenneth-w-chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2023-11-13 11:14:47 -0600

    Added positions for each stair

--- DP Mystery Map/Assets/Scripts/StageData.cs
+++ DP Mystery Map/Assets/Scripts/StageData.cs
@@ -1,7 +1,22 @@
-﻿namespace PlayerInfo
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace PlayerInfo
 {
-    public class StageData
+    public static class StageData
     {
+        public static readonly ReadOnlyCollection<PlayerPosition> FloorOneToFloorTwoPos = new ReadOnlyCollection<PlayerPosition>(new List<PlayerPosition>()
+        {
+            
+        });
         
+        public static readonly ReadOnlyCollection<PlayerPosition> FloorTwoToFloorOnePos = new ReadOnlyCollection<PlayerPosition>(new List<PlayerPosition>()
+        {
+            new PlayerPosition(new Vector2(-208.5f, 28.5f), Direction.Down),
+            new PlayerPosition(new Vector2(117.5f, 142.5f), Direction.Left),
+            new PlayerPosition(new Vector2(118.5f, 15.5f), Direction.Up),
+            new PlayerPosition(new Vector2(255.5f, -119.5f), Direction.Down),
+        });
     }
 }
