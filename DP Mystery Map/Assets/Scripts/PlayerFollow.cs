3ae307c540be6f5e0b884adf7c77c3c78460dd86
kenneth-w-chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2023-11-13 13:48:13 -0600

    Changed to a Singleton

--- DP Mystery Map/Assets/Scripts/PlayerFollow.cs
+++ DP Mystery Map/Assets/Scripts/PlayerFollow.cs
@@ -8,10 +8,19 @@ public class PlayerFollow : MonoBehaviour
 
     public GameObject playerObject;
 
+    private static PlayerFollow _reference;
 
     private float _zPosition;
     private void Start()
     {
+        if(_reference is not null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _reference = this;
+        DontDestroyOnLoad(this.gameObject);
         _zPosition = transform.position.z;
     }
 
@@ -20,5 +29,11 @@ public class PlayerFollow : MonoBehaviour
     {
         var newPosition = playerObject.transform.position;
         this.transform.position = new Vector3(newPosition.x,newPosition.y, _zPosition);
+    }
+
+    private void OnDestroy()
+    {
+        if (_reference != this)
+            _reference = null;
     }
 }
