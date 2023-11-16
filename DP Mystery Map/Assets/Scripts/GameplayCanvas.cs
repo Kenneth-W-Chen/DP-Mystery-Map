2858609a6406c297f447cf60561e2063f2a71e4c
Kenneth-W-Chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2023-11-15 20:37:47 -0600

    Added a canvas prefab specifically for gameplay elements
    This is a persistent/DontDestroyOnLoad object. These objects will be destroyed when returning to the main menu scene

--- DP Mystery Map/Assets/Scripts/GameplayCanvas.cs
+++ DP Mystery Map/Assets/Scripts/GameplayCanvas.cs
@@ -1,7 +1,24 @@
-﻿namespace PlayerInfo
+﻿using System;
+
+namespace PlayerInfo
 {
-    public class GameplayCanvas
+    public class GameplayCanvas : GameplayScript
     {
-        
+        private static GameplayCanvas reference;
+        protected override void Start()
+        {
+            if (reference is not null)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+            base.Start();
+        }
+
+        private void OnDestroy()
+        {
+            if (reference == this)
+                reference = null;
+        }
     }
 }
