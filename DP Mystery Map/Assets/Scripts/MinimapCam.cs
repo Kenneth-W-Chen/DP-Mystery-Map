d820d2da7fc72986ed9ee55815b739a5b52129a9
Douglas Parks <92005036+RogueRapier@users.noreply.github.com>
2023-12-04 21:33:26 -0600

    Da uh, mini stuff

---
+++ DP Mystery Map/Assets/Scripts/MinimapCam.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PlayerInfo;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MinimapCam : GameplayScript
+{
+
+    public GameObject playerObject;
+
+    private static MinimapCam _reference;
+
+    private float _zPosition;
+
+    protected override void Start()
+    {
+        if (_reference is not null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        base.Start();
+
+        _reference = this;
+        _zPosition = transform.position.z;
+    }
+
+    // LateUpdate is called after all Update methods are finished
+    void LateUpdate()
+    {
+        var newPosition = playerObject.transform.position;
+        this.transform.position = new Vector3(newPosition.x, newPosition.y, _zPosition);
+    }
+
+    private void OnDestroy()
+    {
+        if (_reference != this)
+            _reference = null;
+    }
+}
